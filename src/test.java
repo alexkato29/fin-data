f6de3cb44b9cd268b8ba1957db70992ea6f97241
Daniel Choi <dchoi315@gmail.com>
2019-11-25 12:46:16 -0500

    files deleted? not sure

--- src/test.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by AlexKatopodis on 11/18/19.
- */
-public class test {
-    public static void main(String[] args) {
-        System.out.println("TEST!");
-    }
-}
--- src/test3.java
+++
@@ -1,5 +0,0 @@
-public class test3 {
-    public static void main(String[] args) {
-        System.out.println("Sup DChoi315.  Welcome to the git.");
-    }
-}
