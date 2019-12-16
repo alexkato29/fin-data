ed653406c3e295bc9d24cee05e2f078d3a2a1cd4
Daniel Choi <dchoi315@gmail.com>
2019-12-16 13:13:30 -0500

    gui changes

---
+++ src/jsonData.java
@@ -0,0 +1,30 @@
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+
+public class jsonData implements EventHandler<ActionEvent> {
+
+
+    private Stage masterWindow;
+
+    public jsonData(Stage masterWindow) {
+        this.masterWindow = masterWindow;
+
+    }
+
+    @Override
+    public void handle(ActionEvent event) {
+
+        FlowPane test = new FlowPane();
+
+
+//        Add a go Back Button
+        Scene portfolioScene = new Scene(test,300, 500);
+        masterWindow.setScene(portfolioScene);
+
+    }
+}
