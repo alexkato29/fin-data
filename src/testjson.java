3630215792bbf2e5467abbfb6e45b3aba1a6b1ab
Daniel Choi <dchoi315@gmail.com>
2020-01-09 13:25:46 -0500

    updating db complete.

--- src/testjson.java
+++ src/testjson.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 public class testjson {
 
     public static void main(String[] args) {
-        try (Reader reader = new FileReader(".\\data\\newJSON.json")) {
+        try (Reader reader = new FileReader(".\\data\\testJSON.json")) {
 
             JSONParser parser = new JSONParser();
             JSONObject data = (JSONObject) parser.parse(reader);
@@ -17,28 +17,28 @@ public class testjson {
 
             ArrayList<Trade> tradeList = readData.csvParse(".\\data\\test.csv");
 
-
+            System.out.println(data);
             for (int i = 0; i < portfolios.size(); i++){
                 JSONObject account = (JSONObject) portfolios.get(i);
                 for (int j = 0; j < tradeList.size(); j++){
                     Trade trade = tradeList.get(j);
+//                    System.out.println(account.get("securities") + " = " + trade.getTicker());
                     if (account.get("accountNum").equals(trade.getAccountNum())) {
                         JSONObject securities = (JSONObject) account.get("securities");
 
                         JSONObject company = (JSONObject) securities.get(trade.getTicker());
-                        System.out.println(trade.getTicker() + " " + company);
+//                        System.out.println(trade.getTicker() + " " + company);
 
                         double newQuantity = (long)company.get("quantity") + trade.getQuantityPurchased();
                         double newPrice = trade.getPrice();
+                        System.out.println();
                         company.put("quantity", 9999);
                         company.put("price", 9999);
-
-                        System.out.println(company);
                     }
                 }
             }
 
-            FileWriter file = new FileWriter(".\\data\\testJSON.json", true);
+            FileWriter file = new FileWriter(".\\data\\output.json", true);
             try {
                 file.write(data.toJSONString());
 
