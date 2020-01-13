3e3258b5d637cf7a60b98e31b453a3fcb44297b1
Alex Katopodis <alexkato29@gmail.com>
2020-01-13 13:21:03 -0500

    Merge branch 'master' of https://github.com/alexkato29/fin-data
    # Conflicts:
    #	src/Portfolio.java

--- src/Database.java
+++ src/Database.java
@@ -1,23 +1,31 @@
 import jdk.nashorn.internal.parser.JSONParser;
 import org.json.simple.JSONObject;
 
+import java.util.Map;
+
 /**
  * Created by AlexKatopodis on 1/2/20.
  */
 public class Database {
     private String pathToFile;
+    private Map<String, Portfolio> portfolios;
 
     public Database (String pathToFile) {
         this.pathToFile = pathToFile;
+
     }
 
+//    private Map<String, Portfolio> createNewDB () {
+//
+//    }
+//
 //    public Portfolio getPortfolio (String accountNum) {
 //    }
 //
 //    public boolean updatePortfolio (Portfolio p) {
 //        return isSuccessful;
 //    }
-
+//
 //    public boolean newPortfolio (Portfolio p) {
 //        return isSuccessful;
 //    }
