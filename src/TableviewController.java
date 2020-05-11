5b56b81139796ea89c57cb0b1e40e96b5bbb3e42
junghole <dchoi315@gmail.com>
2020-05-11 12:19:52 -0400

    added listeners

--- src/TableviewController.java
+++ src/TableviewController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -41,8 +42,19 @@ public class TableviewController implements Initializable {
 
         ObservableList<Row> rows = FXCollections.observableArrayList();
 
-        for (Portfolio p : portfolioDatabase.getPortfolios().values())
-            rows.add(new Row(p.getAccountHolder(), p.getAccountNum()));
+
+        for (Portfolio p : portfolioDatabase.getPortfolios().values()){
+
+            Button pbutton  = new Button("Portfolio");
+            pbutton.setOnAction(this::downloadPortfolio);
+            Button tbutton  = new Button("Trade Log");
+            tbutton.setOnAction(this::downloadTL);
+            Button dbutton  = new Button("Delete");
+            dbutton.setOnAction(this::delete);
+            rows.add(new Row(p.getAccountHolder(), p.getAccountNum(), pbutton, tbutton, dbutton ) );
+//            rows.add(new Row(p.getAccountHolder(), p.getAccountNum(), new Button("hello"), new Button("hello"), new Button("hello") ) );
+        }
+
 
         names.setCellValueFactory(new PropertyValueFactory<Row, String>("name"));
         nums.setCellValueFactory(new PropertyValueFactory<Row, String>("accountNum"));
@@ -56,15 +68,24 @@ public class TableviewController implements Initializable {
     @FXML
     public void downloadPortfolio(ActionEvent event) {
         // TODO: Make the button download the given portfolio's CSV using the method "export" inside of portfolio.
+        System.out.println("p");
+        System.out.println(event.getSource());
+        System.out.println(event);
     }
 
     @FXML
     public void downloadTL(ActionEvent event) {
         // TODO: Make this button download the Trade Log for the given portfolio using the ExportTrades class
+        System.out.println("t");
+        System.out.println(event.getSource());
+        System.out.println(event);
     }
 
     @FXML
     public void delete(ActionEvent event){
         // TODO: Make this button delete a portfolio using the "delete" method in Database
+        System.out.println("d");
+        System.out.println(event.getSource());
+        System.out.println(event);
     }
 }
